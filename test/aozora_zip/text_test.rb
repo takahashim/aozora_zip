659fb8815c41cab0378664a990ab111ca5267817
takahashim <maki@rubycolor.org>
2020-01-05 15:44:47 +0900

    add another content

--- test/aozora_zip/text_test.rb
+++ test/aozora_zip/text_test.rb
@@ -25,6 +25,11 @@ module AozoraZip
       assert_equal "test/fixtures/unzip/59300_ruby_69898/nekoto_shozoto_futarino_onna_bunko.txt", @text.absolute_path
     end
 
+    def test_absolute_path2
+      text = AozoraZip::Core.new("test/fixtures/unzip/59636_ruby_69900").text
+      assert_equal "test/fixtures/unzip/59636_ruby_69900/narazumono.txt", text.absolute_path
+    end
+
     def test_create_date_string_short
       assert_equal "2020年1月2日作成", @text.create_date_string(Time.new(2020,1,2))
     end
@@ -35,6 +40,11 @@ module AozoraZip
 
     def test_find_created_date
       assert_equal Time.new(2019,12,27), @text.find_created_date
+    end
+
+    def test_find_created_date2
+      text = AozoraZip::Core.new("test/fixtures/unzip/59636_ruby_69900").text
+      assert_equal Time.new(2019,12,27), text.find_created_date
     end
 
     def test_update_created_date
